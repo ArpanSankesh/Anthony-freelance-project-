bf86db0417a8896eee3ab66f8a3b911672c05264
ArpanSankesh <arpansankesh830@gmail.com>
2024-03-22 16:35:00 +0530

    updates

--- script.js
+++ script.js
@@ -3,13 +3,19 @@ const hourSelector = document.querySelectorAll(".hour-selector");
 const workerSelector = document.querySelectorAll(".professional-selector");
 const workerCount = document.querySelector(".workerCount");
 const timeSelector = document.querySelectorAll(".time-selector");
+const addOnSelector = document.querySelectorAll(".add-on-selector");
 const timeCount = document.querySelector(".timeCount");
 const serviceCount = document.querySelector(".serviceCount");
+const savebtn = document.querySelector(".save-btn");
+const Address = document.querySelector(".address");
+const AddressCount = document.querySelector(".addressCount");
 
 let hours = "";
 let workers = "";
 let timming = "";
 let serviceCounter = "";
+let addressCounter = "";
+let addOnCounter = 0;
 
 
 let selectService = () => {
@@ -23,6 +29,21 @@ let selectService = () => {
   });
 };
 selectService();
+
+let addOn = () => {
+  addOnSelector.forEach((addOn) => {
+    addOn.addEventListener("click", () => {
+      if(addOnCounter === 0){
+        addOn.classList.add("active");
+        addOnCounter = 1;
+      }else{
+        addOn.classList.remove("active");
+        addOnCounter = 0;
+      }
+    });
+  });
+};
+addOn();
 
 let selectHour = () => {
   hourSelector.forEach((hour) => {
@@ -56,3 +77,10 @@ let selectTime = () => {
   });
 };
 selectTime();
+
+let saveInfo = () => {
+    addressCounter =  Address.value;
+     AddressCount.innerHTML = addressCounter;
+    console.log(addressCounter);
+};
+savebtn.addEventListener('click', saveInfo)
